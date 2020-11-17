f748396ea615cc6a90818ea542a68510b715ff20
karine sobrinho <karine.silva.sobrinho@gmail.com>
2020-11-17 18:39:30 -0300

    botao de signIn

--- lib/screens/login/loginScreen.dart
+++ lib/screens/login/loginScreen.dart
@@ -1,5 +1,6 @@
 import 'package:animacoes_complexas/screens/login/widgets/FormContainer.dart';
 import 'package:animacoes_complexas/screens/login/widgets/SignUpButton.dart';
+import 'package:animacoes_complexas/screens/login/widgets/StaggerAnimation.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -7,17 +8,33 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('images/background.jpg'), 
-                fit: BoxFit.cover
-              )
-            ),
+                image: AssetImage('images/background.jpg'), fit: BoxFit.cover)),
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
@@ -28,16 +45,20 @@ class _LoginScreenState extends State<LoginScreen> {
                   children: <Widget>[
                     Padding(
                       padding: EdgeInsets.only(top: 70, bottom: 32),
-                      child: Image.asset("images/tickicon.png",
+                      child: Image.asset(
+                        "images/tickicon.png",
                         width: 150,
                         height: 150,
                         fit: BoxFit.contain,
                       ),
                     ),
-                   FormContainer(),
-                   SignUpButton(),
+                    FormContainer(),
+                    SignUpButton(),
                   ],
                 ),
+                StaggerAnimation(
+                  controller: _animationController.view,
+                )
               ],
             )
           ],
